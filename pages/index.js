63feb040b6006406b47f76507d043ed83ab02315
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2022-01-25 18:39:10 -0300

    trocando background

--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ export default function PaginaInicial() {
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          backgroundImage: 'url(https://images.unsplash.com/photo-1638285852084-e4add189d132?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
